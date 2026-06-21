df589d797da3f1400e8a96f4d3983c1e9b2904c4
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-06-21 14:27:47 +0300

    Update version-manager.service.ts

--- apps/generator-cli/src/app/services/version-manager.service.ts
+++ apps/generator-cli/src/app/services/version-manager.service.ts
@@ -16,11 +16,13 @@ import { spawn, spawnSync } from 'child_process';
 
 if (process.env.OPENAPI_GENERATOR_CLI_SEARCH_URL === 'DEFAULT' ) {
      this.logger.log(chalk.red("qqqqq OPENAPI_GENERATOR_CLI_SEARCH_URL is default"))
-      return this.getObservableVersions();
+        console.log("qqqqq OPENAPI_GENERATOR_CLI_SEARCH_URL is default");
+    } else {
+          this.logger.log(chalk.red("qqqqq OPENAPI_GENERATOR_CLI_SEARCH_URL is not default"))
+        console.log("qqqqq OPENAPI_GENERATOR_CLI_SEARCH_URL is not default")
     }
 
-    this.logger.log(chalk.red("qqqqq OPENAPI_GENERATOR_CLI_SEARCH_URL is not default"))
-
+     
 export interface Version {
   version: string;
   versionTags: string[];
