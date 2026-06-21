3d94c1c717f4f2e2497b70a13166dc0f634f3151
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2026-06-21 16:07:14 +0300

    Update main.ts

--- apps/generator-cli/src/main.ts
+++ apps/generator-cli/src/main.ts
@@ -6,6 +6,12 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app/app.module';
 
+if (process.env.OPENAPI_GENERATOR_CLI_SEARCH_URL === 'DEFAULT' ) {
+  console.log("aaaaa OPENAPI_GENERATOR_CLI_SEARCH_URL === DEFAULT");
+} else {
+  console.log("aaaaabbbbb OPENAPI_GENERATOR_CLI_SEARCH_URL !== DEFAULT");
+}
+
 async function bootstrap() {
   await NestFactory.createApplicationContext(AppModule, {logger: false});
 }
